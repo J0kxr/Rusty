d1fb35d4f113439b99a4ac52b59c7f6275cd254d
Torben Grötzinger <torbeng@Air-von-Torben.fritz.box>
2024-11-23 00:14:09 +0100

    Sectin 2 guessing game

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,37 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
-
 
 fn main() {
     println!("!Zahlenraten!");
 
-    
-    println!("Gib eine Zahl ein: ");
+    let number_to_guess = rand::thread_rng().gen_range(1..=100);
 
+    println!("Die Geheimzahl ist: {number_to_guess}");
 
-    let mut guess = String::new();
+    loop {
+        println!("Gib eine Zahl ein: ");
 
+        let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Fehler beim lesen der Zeile!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Fehler beim lesen der Zeile!");
 
-    println!("Deine Schätzung ist die Zahl: {}", guess);
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
+        println!("Deine Schätzung ist die Zahl: {}", guess);
+
+        match guess.cmp(&number_to_guess) {
+            Ordering::Less => println!("Zu klein!"),
+            Ordering::Greater => println!("Zu groß!"),
+            Ordering::Equal => {
+                println!("Du hast gewonnen!");
+                break;
+            }
+        }
+    }
 }
